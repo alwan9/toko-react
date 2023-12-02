a8eeea6c5723357f54301a85c07e4452b86e0458
alwan9 <hafizsusilo86@gmail.com>
2023-12-03 00:02:52 +0700

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,10 @@ import { useNavigate } from "react-router-dom";
 import Navbar from "../components/navbar";
 import Comment from "../components/comment";
 import rog from "../img/rog.png";
+import k1 from "../img/card.png";
 import k2 from "../img/jamin.png";
 import k3 from "../img/box.png";
-import k1 from "../img/card.png";
+import k4 from "../img/pengiriman.png";
 
 export default function About() {
   return (
@@ -13,16 +14,18 @@ export default function About() {
       <Navbar />
       <div className="lg:grid lg:grid-cols-2 px-10 items-center my-[13%]">
         <img src={rog} className="px-[20%]" alt="" />
-        <div className="lg:pl-[10%] lg:pr-[20%]">
-          <div className="text-red-500 text-6xl font-bold">Peringatan</div>
-          <div className="text-2xl mt-10">
+        <div className="lg:pl-[10%] lg:pr-[20%] text-center md:text-left">
+          <div className="text-red-500 text-3xl md:text-6xl mt-16 md:mt-0 font-bold">
+            Peringatan
+          </div>
+          <div className="text-2xl mt-2 md:mt-10">
             website ini hanya sebuah project sederhana yang dibuat oleh alwan.
             website ini bukan website resmi dari Brand Asus ROG
           </div>
         </div>
       </div>
 
-      <div className=" w-full px-[15%] bg-zinc-900 my-40">
+      <div className=" w-full lg:px-[15%] bg-zinc-900 my-40">
         <iframe
           className="w-[100%] h-[30%] lg:h-[70%] "
           src="https://www.youtube.com/embed/qWgkUaHAa2c?si=YQ7QhsXwL7P6ovGB"
@@ -35,25 +38,26 @@ export default function About() {
       <div className="text-center text-xl lg:text-6xl">
         Terimakasih & Selamat menikmati
       </div>
-      <div className="lg:grid lg:gap-10 lg:grid-cols-3 items-center px-7 lg:px-16 my-20 text-center">
-        <div className="lg:px-10">
-          <img className="w-[100%] px-16 lg:px-32 " src={k1} />
-          <div className="text-base lg:text-2xl mt-10 px-5 lg:px-24">
-            pembelian yang terbilang mudah dan terjamin keamanannya
+      <div className="grid gap-5 lg:gap-10 px-20 lg:px-32 grid-cols-2 mt-20 lg:grid-cols-4 items-center   text-center">
+        <div className="bg-blur py-7 w-[100%] h-[100%] px-10 lg:px-16 mt-5  ">
+          <img className="w-[100%] px-[2%]  " src={k1} />
+          <div className=" text-2xl mt-10 ">Pembayaran yang mudah dan aman</div>
+        </div>
+        <div className="bg-blur py-7 w-[100%] h-[100%] px-10 lg:px-16 mt-5  ">
+          <img className="w-[100%] px-[2%]  " src={k2} />
+          <div className=" text-2xl mt-10 ">
+            produk yang kami jual 100% produk asli
           </div>
         </div>
-        <div className="lg:px-10">
-          <img className="w-[100%] px-16 lg:px-32 " src={k2} />
-          <div className="text-base lg:text-2xl mt-10 px-5 lg:px-24">
-            produk yang kami jual 100% produk asli dari pabrik asus
+        <div className="bg-blur py-7 w-[100%] h-[100%] px-10 lg:px-16 mt-5  ">
+          <img className="w-[100%] px-[2%]  " src={k3} />
+          <div className=" text-2xl mt-10 ">
+            Packing yang rapi dan aman sampai tujuan
           </div>
         </div>
-        <div className="lg:px-10">
-          <img className="w-[100%] px-16 lg:px-32 " src={k3} />
-          <div className="text-base lg:text-2xl mt-10 px-5 lg:px-24">
-            terdapat banyak pilihan produk asus mulai dari laptop gaming hingga
-            aksesoris gaming{" "}
-          </div>
+        <div className="bg-blur py-7 w-[100%] h-[100%] px-10 lg:px-16 mt-5  ">
+          <img className="w-[100%] px-[2%]  " src={k4} />
+          <div className=" text-2xl mt-10 ">Proses pengiriman yang cepat</div>
         </div>
       </div>
       <Comment />
